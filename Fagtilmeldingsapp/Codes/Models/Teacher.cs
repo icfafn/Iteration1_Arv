615029d4317bf7599ab86866836e85ae6b9271fe
Kristian Pedersen <Icfafn@gmail.com>
2022-02-21 12:40:50 +0100

    Mandag Slut

---
+++ Fagtilmeldingsapp/Codes/Models/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fagtilmeldingsapp.Codes.Models
+{
+    internal class Teacher
+    {
+        public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+    //List<Teacher> lstTeachers = new()
+    //{
+    //    { Id = 1, FirstName = "Niels", LastName = "Olesen" },
+    //    { Id = 2, FirstName = "Henrik", LastName = "Poulsen" },
+    //    { Id = 2, FirstName = "Tanja", LastName = "Olesen" }
+
+    //}
+}
---
+++ Fagtilmeldingsapp/Codes/Models/Validation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fagtilmeldingsapp.Codes.Models
+{
+    internal class Validation
+    {
+    }
+}
