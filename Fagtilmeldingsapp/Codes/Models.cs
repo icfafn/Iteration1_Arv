f9515cecd7db49e3d4adc68a90d999dbfe275409
icfafn <95622845+icfafn@users.noreply.github.com>
2022-02-24 12:24:11 +0100

    Create Models.cs

---
+++ Fagtilmeldingsapp/Codes/Models.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{
+    internal class Teacher
+    {
+        public int LærerId { get; set; }
+        public string? ForNavn { get; set; }
+        public string? EfterNavn { get; set; }
+    }
+    internal class Student
+    {
+        public int ElevId { get; set; }
+        public string? ForNavn { get; set; }
+        public string? EfterNavn { get; set; }
+    }
+    internal class Course
+    {
+        public int CourseId { get; set; }
+        public string? CourseName { get; set; }
+        public int TeacherId { get; set; }
+    }
+    internal class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int? ElevId { get; set; }
+        public int? CourseId { get; set; }
+    }
+}
