d5de4ab7c8fa9b4868bedde32e45c4fdcc05a432
icfafn <95622845+icfafn@users.noreply.github.com>
2022-02-24 12:23:50 +0100

    Update School.cs

--- Fagtilmeldingsapp/Codes/School.cs
+++ Fagtilmeldingsapp/Codes/School.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace FagTilmeldingApp.Codes
 {
-    //abstract indkapsler School så den kun kan være parentklasse.
-    internal abstract class School 
+    internal abstract class School
     {
-        public string SchoolName { get; set; }
+        public string? SchoolName { get; set; }
 
-        public string UddannelsesLinjen {get; set;}
+        public string? UddannelseslinjeBeskrivelse { get; set; }    
 
-        public School (string schoolName)
+        public School(string schoolName)
         {
             SchoolName = schoolName;
         }
-       
-        //hvis man ikke ved hvad kroppen skal være, men kender metoden. Derfor abstract så jeg laver child lave kroppen.
-        public abstract void SetUddannelsesLinje(string? uddannelsesLinje);
+        public abstract void SetUddannelsesLinje(string? uddannelseslinje);
+
+        public abstract void SetUddannelsesLinje(string? uddannelseslinje, string? uddannelsesbeskrivelse);
+
     }
 }
