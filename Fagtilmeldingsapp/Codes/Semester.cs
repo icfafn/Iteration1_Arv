d9a9d4bacb5ce884e9484e63ea16e6633236b2d7
icfafn <95622845+icfafn@users.noreply.github.com>
2022-02-24 12:03:37 +0100

    Update Semester.cs

--- Fagtilmeldingsapp/Codes/Semester.cs
+++ Fagtilmeldingsapp/Codes/Semester.cs
@@ -1,24 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fagtilmeldingsapp.Codes
+namespace FagTilmeldingApp.Codes
 {
-    internal class Semester : School
+    //Sealed betyder det er den sidste i nedarving.
+    //Semester : School betyder at Semester er en nedarving af School.
+    internal sealed class Semester : School
     {
-        public string? SemesterNavn { get; set; }
+        public string SemesterNavn { get; set; }
         public string? UddannelsesLinje { get; set; }
-         public Semester(string? schoolName,string? semesterNavn) : base(schoolName)
+        public string? UddannelsesLinjeBeskrivelse { get; set; }
+        //base = parent. ved at skrive base ved construkter, betyder at data kan komme fra parent til child og omvendt
+        //jeg har schoolName med fordi den kommer fra parentklasse.
+        public Semester(string semesterNavn, string schoolName) : base(schoolName)
         {
             SemesterNavn = semesterNavn;
         }
-
-        public override void SetUddannelseslinje(string? UddannelsesLinje)
+        //bliver brugt til at kunne tilføje en ekstra propperty uden at ødelægge parent og child struktur eller metode.
+        public override void SetUddannelsesLinje(string? uddannelsesLinje)
         {
-            UddannelsesLinje = uddannelseslinje;
+            UddannelsesLinje = uddannelsesLinje;
+        }
+
+        public void SetUddannelsesLinje(string uddannelsesLinje, string uddannelsesLinjeBeskrivelse)
+        {
+            UddannelsesLinje = uddannelsesLinje;
+            UddannelsesLinjeBeskrivelse = uddannelsesLinje;
         }
     }
-
 }
