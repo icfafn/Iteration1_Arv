efbb356275c493eb4a5c7f3c5fc4cc18530e35c9
icfafn <95622845+icfafn@users.noreply.github.com>
2022-02-24 12:44:21 +0100

    Update Program.cs

--- Fagtilmeldingsapp/Program.cs
+++ Fagtilmeldingsapp/Program.cs
@@ -7,11 +7,11 @@ string AngivLinje;
 string AngivBeskrivelse;
 ConsoleKeyInfo cki;
 
-Console.Write("Angiv skole: ");
+Console.Write("Angiv Skole: ");
 AngivSkole = Console.ReadLine();
-Console.Write("Angiv hovedforløb: ");
+Console.Write("Angiv Hovedforløb: ");
 AngivForløb = Console.ReadLine();
-Console.Write("Angiv uddannelseslinje: ");
+Console.Write("Angiv Uddannelseslinje: ");
 AngivLinje = Console.ReadLine();
 
 Semester s = new(AngivSkole, AngivForløb);
@@ -19,9 +19,9 @@ Semester s = new(AngivSkole, AngivForløb);
 do
 {
     Console.Clear();
-    Console.WriteLine("Angiv skole: " + AngivSkole);
-    Console.WriteLine("Angiv hovedforløb: " + AngivForløb);
-    Console.WriteLine("Angiv uddannelseslinje: " + AngivLinje);
+    Console.WriteLine("Angiv Skole: " + AngivSkole);
+    Console.WriteLine("Angiv Hovedforløb: " + AngivForløb);
+    Console.WriteLine("Angiv Uddannelseslinje: " + AngivLinje);
     Console.WriteLine("\nØnsker du at angiv en kort beskrivelse af uddannelseslinje?:  ");
     Console.WriteLine("1) Ja");
     Console.WriteLine("2) Nej");
@@ -94,7 +94,7 @@ while (mainflag)
     foreach (Student student in students)
     {
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.Write(student.ForNavn + " " + student.EfterNavn + " tilmeldt fag ");
+        Console.Write(student.ForNavn + " " + student.EfterNavn + " Tilmeldt fag ");
     }
     foreach (Course course in courses)
     {
@@ -137,7 +137,7 @@ while (mainflag)
     while (flag)
     {
         Console.ForegroundColor = ConsoleColor.White;
-        Console.Write("\nKurse ID: ");
+        Console.Write("\nKursus ID: ");
         try
         {
             UserCourseId = Convert.ToInt32(Console.ReadLine());
@@ -151,7 +151,7 @@ while (mainflag)
             else if (valid3.Count == 0)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Kursen findes ikke");
+                Console.WriteLine("Kursus findes ikke");
             }
         }
         catch
@@ -169,7 +169,7 @@ while (mainflag)
     else
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("\nStudent already exist in that class - Try again!");
+        Console.WriteLine("\nEleven findes allerede i denne klasse");
         UserElevId = 0;
         UserCourseId = 0;
         Console.ReadKey();
