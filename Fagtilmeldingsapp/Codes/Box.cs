221b2e98bdf34e8d8a7a885bb0f08921bc0570a2
Kristian Pedersen <Icfafn@gmail.com>
2022-02-25 09:56:54 +0100

    Første push

---
+++ Fagtilmeldingsapp/Codes/Box.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fagtilmeldingsapp.Codes;
+
+namespace Fagtilmeldingsapp.Codes
+{
+    internal class Box : IComparable<Box>
+    {
+        private double Højde { get; set; }
+
+        private double Bredde { get; set; }
+
+        private double Længde { get; set; }
+
+        public double Volume { get; set; }
+
+        private BoxType Type { get; set; }
+
+        public Box()
+        {
+
+        }
+
+        public Box(double højde, double bredde, double længde)
+        {
+            Højde = højde;
+            Bredde = bredde;
+            Længde = længde;
+            GetVolume();
+            SetBoxType();
+        }
+
+
+        private void GetVolume()
+        {
+            Volume = Længde * Bredde * Højde;
+        }
+
+        public override string ToString()
+        {
+            return $"Volume {Volume}, {Type.ToString()}";
+        }
+
+        public static Box operator +(Box b, Box c)
+        {
+            Box box = new Box();
+
+            //Indhold
+            box.Længde = b.Længde + c.Længde;
+            box.Bredde = b.Bredde + c.Bredde;
+            box.Højde = b.Højde + c.Højde;
+            box.Volume = box.Længde * box.Bredde * box.Højde;
+
+            // Lillebox + lillebox giver mellembox, resten giver storbox
+            if(b.Type == BoxType.lilleBox && c.Type == BoxType.lilleBox)
+            {
+                box.Type = BoxType.mellemBox;
+            }
+            else
+            {
+                box.Type = BoxType.storBox;
+            }
+            return box;
+        }
+
+        private void SetBoxType()
+        {
+            if (Volume <= 125)
+            {
+                // lille box
+                Type = BoxType.lilleBox;
+            }
+            else if(Volume >= 126 && Volume <=1500)
+            {
+                // Mellem box
+                Type = BoxType.mellemBox;
+            }
+            else
+            {
+                // Stor box
+                Type = BoxType.storBox;
+            }
+
+            //Anden måde at skrive det på. 
+            //Type = (Volume <= 125) ? BoxType.lilleBox : (Volume >= 126 && Volume <= 1500) ? BoxType.mellembox : storbox;
+        }
+
+        public int CompareTo(Box? other)
+        {
+            if(Volume < other.Volume)
+            {
+                return 1;
+            }
+            else if(Volume > other.Volume)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+            //return (Volume <other.Volume) ? 1 : (Volume > other.Volume) ? -1 : 0;
+        }
+        //Type(enum boxType)
+    }
+}
--- Fagtilmeldingsapp/Program.cs
+++ Fagtilmeldingsapp/Program.cs
@@ -1,44 +1,29 @@
-using FagTilmeldingApp.Codes;
+using Fagtilmeldingsapp.Codes;
 
+Box a = new Box(5.0, 5.0, 5.0);
+Console.WriteLine($"Box a : {a.ToString()}");
 
-List<Course> KurseList = new()
-{   
-    new Course() { CourseId = 9, CourseName = "Grundlæggende Programmering", TeacherId = 1 },
-    new Course() { CourseId = 7, CourseName = "Database Programmering", TeacherId = 1 },
-    new Course() { CourseId = 1, CourseName = "Studieteknik", TeacherId = 1 },
-    new Course() { CourseId = 5, CourseName = "Clientside Programmering", TeacherId = 2 }
+Console.WriteLine();
+
+Box b = new Box(4.0, 4.0, 4.0);
+Console.WriteLine($"Box b : {b.ToString()}");
+
+Console.WriteLine();
+
+Box c = a + b;
+Console.WriteLine($"Box c : {c.ToString()}");
+
+List<Box> boxes = new()
+{
+    a,
+    b,
+    c
 };
 
-Console.WriteLine("Usorteret liste: ");
-Console.WriteLine("-----------------------------------------------------------------");
+boxes.Sort();
+boxes.Reverse();
 
-foreach (Course c in KurseList)
-{
-    Console.WriteLine(c.CourseId + ". " + c.CourseName);
-}
+foreach (Box item in boxes)
+    Console.WriteLine($"Volume : {item.Volume}");
 
-Console.WriteLine("-----------------------------------------------------------------");
-
-Console.WriteLine("Sorteret liste: ");
-Console.WriteLine("-----------------------------------------------------------------");
-
-KurseList.Sort();
-
-foreach (Course b in KurseList)
-{
-    Console.WriteLine(b.CourseId + ". " + b.CourseName);
-}
-
-Console.WriteLine("-----------------------------------------------------------------");
-
-Console.WriteLine("Bagvendt Sorteret liste: ");
-Console.WriteLine("-----------------------------------------------------------------");
-
-KurseList.Reverse();
-
-foreach (Course b in KurseList)
-{
-    Console.WriteLine(b.CourseId + ". " + b.CourseName);
-}
-
-Console.ReadKey();
+Console.ReadLine();
